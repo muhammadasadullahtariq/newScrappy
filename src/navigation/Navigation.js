0d404de32eb998bac2b4bdec8c452f7d01d69669
Vijay Verma <vijay@Vijays-MacBook-Pro.local>
2021-08-27 06:01:25 +0100

    Initial push

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import {View, Text, StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import PhoneAuthScreen from '../screens/PhoneAuthScreen';
+import OtpVerificationScreen from '../screens/OtpVerificationScreen';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+function Navigation() {
+  return (
+    <NavigationContainer>
+      <StatusBar
+        barStyle="dark-content"
+        hidden={false}
+        backgroundColor="#F7F8FA"
+        translucent={true}
+      />
+      <Stack.Navigator headerMode="none">
+        <Stack.Screen name="PhoneAuthScreen" component={PhoneAuthScreen} />
+        <Stack.Screen
+          name="OtpVerificationScreen"
+          component={OtpVerificationScreen}
+        />
+        <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Navigation;
